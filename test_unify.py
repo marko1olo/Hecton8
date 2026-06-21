0c872bbe1cb71e36d47bf3ed30aba28bf50435bf
marko1olo <barsukdana@gmail.com>
2026-06-22 00:11:11 +0400

    Cleanup: remove temporary files from PR 262

--- test_unify.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open("Assets/Shapes/Scripts/Runtime/Utils/ShapesMath.cs", "r") as f:
-    math_content = f.read()
-
-print("CalcBezierPointCount in ShapesMath: ", "CalcBezierPointCount" in math_content)
