55333adfda4455d51954cf1b85e1c6b60201a60b
Петушков А. <96879250+marko1olo@users.noreply.github.com>
2026-06-24 23:05:40 +0400

    PR #466 (squash)
    Fix LODGroup inspector error in VolumetricLightBeam

--- Assets/VolumetricLightBeam/Scripts/LODBeamGroup.cs
+++ Assets/VolumetricLightBeam/Scripts/LODBeamGroup.cs
@@ -21,9 +21,7 @@ namespace VLB
 
         LODGroup m_LODGroup;
 
-#if UNITY_EDITOR
-        GameObject m_SelectionToRestore = null;
-#endif
+
 
         void Awake()
         {
@@ -51,21 +49,39 @@ namespace VLB
         {
             Debug.Assert(m_LODGroup != null);
 
-            LOD[] lods = m_LODGroup.GetLODs();
-            Debug.Assert(lods != null);
-
 #if UNITY_EDITOR
-            if(lods[lodIdx].renderers == null || lods[lodIdx].renderers.Length == 0)
+            if (!Application.isPlaying)
             {
-                if(renderers != null)
+                var so = new SerializedObject(m_LODGroup);
+                var lodsProp = so.FindProperty("m_LODs");
+                if (lodsProp != null && lodsProp.isArray && lodIdx >= 0 && lodIdx < lodsProp.arraySize)
                 {
-                    // Fix a very weird Unity bug happening on 2021 and higher, where the Unity's LODGroup inspector generates errors when modifying its LOD data.
-                    // The only workaround I found is to deselect the gameobject for a bit of time.
-                    m_SelectionToRestore = Selection.activeGameObject;
-                    Selection.activeGameObject = null;
+                    var lodProp = lodsProp.GetArrayElementAtIndex(lodIdx);
+                    var renderersProp = lodProp.FindPropertyRelative("renderers");
+                    if (renderersProp != null && renderersProp.isArray)
+                    {
+                        renderersProp.ClearArray();
+                        if (renderers != null)
+                        {
+                            renderersProp.arraySize = renderers.Length;
+                            for (int i = 0; i < renderers.Length; i++)
+                            {
+                                var rendererProp = renderersProp.GetArrayElementAtIndex(i);
+                                var rendererRef = rendererProp.FindPropertyRelative("renderer");
+                                if (rendererRef != null)
+                                {
+                                    rendererRef.objectReferenceValue = renderers[i];
+                                }
+                            }
+                        }
+                        so.ApplyModifiedProperties();
+                        return;
+                    }
                 }
             }
 #endif
+            LOD[] lods = m_LODGroup.GetLODs();
+            Debug.Assert(lods != null);
             lods[lodIdx].renderers = renderers;
             m_LODGroup.SetLODs(lods);
         }
@@ -247,12 +263,6 @@ namespace VLB
 #if UNITY_EDITOR
             if (!Application.isPlaying)
             {
-                if (m_SelectionToRestore)
-                {
-                    Selection.activeGameObject = m_SelectionToRestore;
-                    m_SelectionToRestore = null;
-                }
-
                 SetupLodGroupData();
                 UnifyBeamsProperties();
                 return;
