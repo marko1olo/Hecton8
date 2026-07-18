82bd484f0600d46cac93cc76ec7a8af301fa2010
marko1olo <marko1olo@users.noreply.github.com>
2026-07-19 02:26:45 +0400

    chore(backup): auto-commit uncommitted changes

---
+++ run_pom.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Unity\Hub\Editor\6000.5.0f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\hades\Hecton8" -executeMethod "Hecton8.Editor.Diagnostics.EnableTerrainPOMTask.ExecuteHeadless" -logFile "C:\hades\Hecton8\EnableTerrainPOM.log"
