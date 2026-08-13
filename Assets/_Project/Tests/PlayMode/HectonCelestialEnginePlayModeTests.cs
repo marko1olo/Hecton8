e8b97d7ee617777627ad246319b152ce5e17eccd
marko1olo <marko1olo@users.noreply.github.com>
2026-08-13 20:18:04 +0400

    test(celestial): cover runtime time-of-day application

---
+++ Assets/_Project/Tests/PlayMode/HectonCelestialEnginePlayModeTests.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using Hecton8.Celestial;
+using Hecton8.Environment;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
+
+namespace Hecton8.Tests.PlayMode
+{
+    [TestFixture]
+    public sealed class HectonCelestialEnginePlayModeTests
+    {
+        private GameObject _atmosphereObject;
+        private HectonAtmosphereManager _atmosphereManager;
+        private GameObject _engineObject;
+        private HectonCelestialEngine _engine;
+
+        [UnitySetUp]
+        public IEnumerator SetUp()
+        {
+            _atmosphereObject = new GameObject(nameof(HectonAtmosphereManager));
+            _atmosphereManager = _atmosphereObject.AddComponent<HectonAtmosphereManager>();
+
+            _engineObject = new GameObject(nameof(HectonCelestialEngine));
+            _engine = _engineObject.AddComponent<HectonCelestialEngine>();
+
+            yield return null;
+        }
+
+        [UnityTearDown]
+        public IEnumerator TearDown()
+        {
+            if (_engineObject != null)
+                Object.Destroy(_engineObject);
+
+            if (_atmosphereObject != null)
+                Object.Destroy(_atmosphereObject);
+
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TryApplyRuntimeTimeOfDay01_ValidTime_UpdatesTheRuntimeAtmosphere()
+        {
+            const float requestedTimeOfDay = 0.73f;
+
+            Assert.That(_engine.TryApplyRuntimeTimeOfDay01(requestedTimeOfDay), Is.True);
+            Assert.That(_atmosphereManager.TimeOfDay, Is.EqualTo(requestedTimeOfDay).Within(0.0001f));
+
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TryApplyRuntimeTimeOfDay01_NonFiniteTime_RejectsInputWithoutMutatingAtmosphere()
+        {
+            const float knownTimeOfDay = 0.27f;
+            Assert.That(_atmosphereManager.TrySetTimeOfDay(knownTimeOfDay), Is.True);
+            float timeBeforeInvalidRequest = _atmosphereManager.TimeOfDay;
+
+            Assert.That(_engine.TryApplyRuntimeTimeOfDay01(float.NaN), Is.False);
+            Assert.That(_atmosphereManager.TimeOfDay, Is.EqualTo(timeBeforeInvalidRequest).Within(0.0001f));
+
+            yield return null;
+        }
+    }
+}
